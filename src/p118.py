311518a14144da7cce8cdc9d5b8f5be674b67996
Chris Liu <guohong.liu@gmail.com>
2012-07-22 14:42:49 +0800

    #118 solved.

--- src/p118.py
+++ src/p118.py
@@ -7,34 +7,14 @@ Created on 2012-7-18
 import mtools
 import unittest
 
-def search_8():
-    l = range(1,10)
-    pl = [2,3,5,7]
-    cnt = 0
-    for x in pl:
-        l2 = l[:]
-        del l2[x-1]
-        for np in mtools.dict_perm(l2):
-            n = mtools.combine_digits(np)
-            if mtools.is_prime(n):
-                print x, n
-                cnt += 1
-    return cnt
-
-def make_dict():
-    primes = mtools.sieve_primes(10**7)
-    print len(primes)
-
-def search_other():
-    d = make_dict()
-
 def trunc_number(n, i, j):
     l = []
     while j >= i:
         s = mtools.separate_digits(n)
         m = int(mtools.combine_digits(s[:i]))
         r = int(mtools.combine_digits(s[i:]))
-        l.append((m, r))
+        if r > m:
+            l.append((m, r))
         i += 1
     return l
 
@@ -46,7 +26,6 @@ def count_partition(n):
     cnt = 0
     ls = []
     while stack:
-        #print stack
         p, r, l = stack.pop()
         j = mtools.count_digits(p)
         k = mtools.count_digits(r)
@@ -55,20 +34,19 @@ def count_partition(n):
             if m > p and is_prime(m):
                 l2.append(m)
                 stack.append((m, r1, l2))
-                if is_prime(r1):
-                    l2.append(r1)
+                if r1 > m and is_prime(r1):
+                    l3 = l2[:]
+                    l3.append(r1)
                     cnt += 1
-                    ls.append(l2)
+                    ls.append(l3)
     return cnt, ls
 
 def hashable(l):
-    l.sort()
     return ','.join([str(x) for x in l])
 
 def count():
     l = range(1,10)
     cnt = 0
-    i = 0
     sets = []
     for np in mtools.dict_perm(l):
         if np[-1] in [2,4,5,6,8]:
@@ -79,7 +57,9 @@ def count():
         if c > 0:
             for e in s:
                 sets.append(e)
-        i += 1
+    
+    #output all valid sets and
+    #make sure no duplicates 
     d = {}
     for s in sets:
         h = hashable(s)
@@ -93,28 +73,17 @@ def count():
     return cnt
 
 class TestP118(unittest.TestCase):
-    def test_1(self):
-        #n = search_8()
-        #print "number of (1,8) sets=", n
-        #n = search_other()
-        #print "number of other sets=", n
-        #n = count_partition(254789631)
-        #print n
-        pass
-
     def test_trunc_number(self):
         numbers = trunc_number(254789631, 1, 4)
         self.assertEqual(4, len(numbers))
         self.assertEqual((2, 54789631), numbers[0])
         self.assertEqual((2547, 89631), numbers[3])
-        numbers = trunc_number(54789631, 1, 4)
-        print numbers
 
     def test_count_partition(self):
-#        self.assertEqual(1, count_partition(254789631))
-        #self.assertEqual(2, count_partition(978534621))
-        #self.assertEqual(0, count_partition(194236857))
-        pass        
+        c, s = count_partition(127859463)
+        self.assertEqual(0, c)
+        c, s = count_partition(127643859)
+        self.assertEqual(2, c)
 
 class P118:
     def test(self):
@@ -123,7 +92,6 @@ class P118:
 
     def solve(self):
         return count()
-        #pass
 
 if __name__ == "__main__":
     mtools.run(P118())
