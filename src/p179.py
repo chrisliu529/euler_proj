2f0e98e4000047898dcb65f081358c140bba403b
Chris Liu <guohong.liu@gmail.com>
2012-08-18 20:34:58 +0800

    #179 improved.

--- src/p179.py
+++ src/p179.py
@@ -9,19 +9,26 @@ import unittest
 
 def search(upper):
     cnt = 0
-    v1 = mtools.factors_count(2)
-    for n in range(2, upper+1):
-        v2 = mtools.factors_count(n+1)
-        if v1 == v2:
+    l = [0 for x in range(0,upper)]
+    lim = upper/2
+    for i in range(2,lim):
+        for j in range(i,len(l),i):
+            l[j] += 1
+    for i in range(2,len(l)-1):
+        if l[i] == l[i+1]:
             cnt += 1
-        v1 = v2
     return cnt
 
 def get_answer():
     return search(10**7)
+    pass
 
 class TP(unittest.TestCase):
-    pass
+    def test_search(self):
+        #sure results with direct search
+        self.assertEqual(15, search(100))
+        self.assertEqual(118, search(1000))
+        self.assertEqual(10585, search(100000))
 
 class P:
     def test(self):
--- src/t.py
+++ src/t.py
@@ -1,7 +1,10 @@
 def change(l):
     l.append(4)
 
-l = [1,2,3]
-print l
-change(l)
-print l
+def sum_hs(n):
+    s = 0
+    for i in range(1, n+1):
+        s += 1.0/i
+    return s
+
+print sum_hs(10**7)
