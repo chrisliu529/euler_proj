8da72b2d531eb273fb6d7d7ae8e4ac73abb0e8d6
Chris Liu <guohong.liu@gmail.com>
2012-09-11 22:26:34 +0800

    #131 improved.

--- src/p131.py
+++ src/p131.py
@@ -5,29 +5,19 @@ Created on 2012-9-11
 '''
 
 import mtools
-import math
 import unittest
 import time
 
 def solve_n(lim):
-    ns = [x**3 for x in range(600+1)]
-    ps = mtools.sieve_primes(lim)
-    ps.reverse()
+    m = 1
     cnt = 0
-    t = time.time()
-    for p in ps:
-        for n in ns:
-            np = n + p
-            if is_cubic(np):
-                print math.pow(n, 1/3.0), p, time.time() - t
-                cnt += 1
-                break
-    return cnt
-
-def is_cubic(n):
-    t = math.pow(n, 1/3.0)
-    d = abs(t-int(t))
-    return d < 0.00000001 or 1-d < 0.00000001 
+    while True:
+        p = 3*m*m + 3*m + 1
+        if p >= lim:
+            return cnt
+        if mtools.is_prime(p):
+            cnt += 1
+        m += 1
 
 def solve():
     return solve_n(10**6)
@@ -35,13 +25,6 @@ def solve():
 class TP(unittest.TestCase):
     def test_solve_n(self):
         self.assertEqual(4, solve_n(100))
-    
-    def test_is_cubic(self):
-        self.assertTrue(is_cubic(8))
-        self.assertTrue(is_cubic(409**3))
-        self.assertFalse(is_cubic(409**3+5))
-        self.assertFalse(is_cubic(4099**3+5))
-        self.assertTrue(is_cubic(4099**3))
 
 if __name__ == "__main__":
     t = time.time()
