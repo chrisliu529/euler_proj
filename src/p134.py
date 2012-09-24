29de146e132c20a462a054c6b57a5b0f3cdd1bff
Chris Liu <guohong.liu@gmail.com>
2012-09-24 23:47:13 +0800

    #134 checkpoint.

--- src/p134.py
+++ src/p134.py
@@ -7,17 +7,50 @@ Created on 2012-9-23
 import mtools
 import unittest
 import time
+import math
 
+def euclidean(a, b):
+    lq = []
+    while b != 1:
+        t = b
+        lq.append(a/b)
+        b = a % b
+        a = t
+    n = len(lq)
+    if n % 2 == 0:
+        return -Q(n, lq), P(n, lq)
+    return Q(n, lq), -P(n, lq)
+
+def P(k, lq):
+    p0 = 1
+    p1 = lq[0]
+    i = 2
+    p = 0
+    while i < k:
+        p = lq[i-1]*p1 + p0
+        p0 = p1
+        p1 = p
+    return p
+
+def Q(k, lq):
+    q0 = 0
+    q1 = 1
+    i = 2
+    q = 0
+    while i < k:
+        q = lq[i-1]*q1 + q0
+        q0 = q1
+        q1 = q
+    return q
+    
 def get_n(p1, p2):
     d = mtools.count_digits(p1)
-    a = 1
     dd = 10**d
-    while True:
-        n = a*dd + p1 
-        if n % p2 != 0:
-            a += 1
-        else:
-            return n
+    _, b0 = euclidean(dd, p2)
+    t = math.floor(-p1*b0/dd)
+    b = - (p1*b0 + dd*t)
+    assert b > 0
+    return b*p2
 
 def sum_n(lim):
     s = 0
